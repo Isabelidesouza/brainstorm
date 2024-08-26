5563930089698c15c46f600cc83ee933094017a7
Isabelidesouza <108525789+Isabelidesouza@users.noreply.github.com>
2024-08-26 11:04:01 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
         enunciado: "Insira o enunciado da Pergunta 1",
